a56d16641d8942213d6635c0e2a8a46228140b81
libin-n-g <libinngeorge@gmail.com>
2017-09-08 00:38:53 +0530

    Completed Write Sys Call

--- spl/run.sh
+++ spl/run.sh
@@ -2,5 +2,6 @@
 echo "Running"
 ./spl --int=1 spl_progs/interrupt1.spl
 ./spl --int=2 spl_progs/interrupt2.spl
+./spl --int=4 spl_progs/interrupt4.spl
 ./spl --int=timer spl_progs/timer_interrupt.spl
 ./spl --os spl_progs/os_startup.spl
