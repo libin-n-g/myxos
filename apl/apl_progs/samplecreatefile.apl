db7092bfda75a6b0c7347f65be58776f2b5f2588
libin-n-g <libinngeorge@gmail.com>
2017-09-07 16:45:07 +0530

    completes Delete Sys Call

--- apl/apl_progs/samplecreatefile.apl
+++ apl/apl_progs/samplecreatefile.apl
@@ -4,7 +4,10 @@ enddecl
 integer main()
 {
 	status = Create("myfile.dat");
-	print ("Status ");
+	print ("Status1 ");
+	print(status);
+	status = Delete("even.xsm");
+	print ("Status2 ");
 	print(status);
 	return 0;
 }
--- spl/run.sh
+++ spl/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Runnning"
-./spl --int=1 spl_progs/system_call.spl
+echo "Running"
+./spl --int=1 spl_progs/interrupt1.spl
 ./spl --int=timer spl_progs/timer_interrupt.spl
 ./spl --os spl_progs/os_startup.spl
